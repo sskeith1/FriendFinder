fbf5dd0835acbcdd3330ef3aa05d411a87dc5c9d
sskeith1 <skeith1@babson.edu>
2017-08-31 16:13:24 -0400

    Fail

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friends = [
+  {
+      "name":"a fish",
+      "photo":"http://s7d2.scene7.com/is/image/PetSmart/ARTHMB-HowDoISetUpMyAquariumForMultipleFish-20160818?$AR1104$",
+      "scores":[
+         5,
+         1,
+         4,
+         4,
+         5,
+         1,
+         2,
+         5,
+         4,
+         1
+          ]
+      },
+  {
+  "name":"Charlie",
+  "photo":"http://cdn.collider.com/wp-content/uploads/2015/02/its-always-sunny-charlie-day-2.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Fred Rogers",
+  "photo":"http://www.thesandspur.org/wp-content/uploads/2015/11/tumblr_m6dr32ysWp1rodz1bo1_1280.jpg",
+  "scores":[
+     9,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+      ]
+},
+{
+  "name":"Betty",
+  "photo":"http://images.huffingtonpost.com/2015-04-28-1430228926-2421958-bettywhiteportraitftr.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+{
+  "name":"Samuel",
+  "photo":"http://www.indiewire.com/wp-content/uploads/2014/02/samuel-l-jackson.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     3,
+     2,
+     6,
+     9,
+     8,
+     9,
+     9
+      ]
+},
+]
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
